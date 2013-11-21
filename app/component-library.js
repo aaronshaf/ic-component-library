368d9dedfd0c8fd1a6c260b282f4454a2f136d15
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-11-21 14:17:04 -0700

    WIP

--- app/component-library.js
+++ app/component-library.js
@@ -6,29 +6,23 @@ var App = Ember.Application.create({
 });
 
 window.components = [
-  {
-    name: 'icon'
-  },
-  {
-    name: "lazily-paginated-records"
-  },
-  {
-    name: "canvas-logo"
-  },
-  {
-    name: "modal"
-  },
-  {
-    name: "menu"
-  },
-  {
-    name: "trigger"
-  }
+  { name: 'ic-icon' },
+  { name: "ic-lazily-paginated-records" },
+  { name: "ic-canvas-logo" },
+  { name: "ic-modal" },
+  { name: "ic-menu" },
+  { name: "ic-button" },
+  { name: "ic-trigger" }
 ];
 
 components = components.map(function(component) {
   component.slug = Ember.String.dasherize(component.name);
+  component.page = 'pages/' + component.slug;
   return Ember.Object.create(component);
+});
+
+Ember.View.reopen({
+  attributeBindings: ['list','autofocus']
 })
 
 App.ApplicationController = Ember.Controller.extend({
@@ -49,15 +43,31 @@ App.IcComponentsRoute = Ember.Route.extend({
   }
 });
 
+App.IcCanvasLogoComponent = Ember.Component.extend({
+  attributeBindings: 'style',
+  'fillColor': function() {
+    return this.get('fill-color') ? this.get('fill-color') : '#D64027';
+  }.property('fill-color')
+});
+
 App.IcComponentsComponentRoute = Ember.Route.extend({
   model: function(params) {
     var component = components.findProperty('slug',params.component_slug);
-    if(!component.page) {
-      jQuery.ajax('pages/ic/' + params.component_slug + '.html?buster=' + Math.random()).then(function(page) {
-        this.set('page',page);
-      }.bind(component));  
-    }
+    component
     return component;
+  },
+  renderTemplate: function(controller,model) {
+    this.render('ic-components/' + model.get('slug'), {
+      view: 'IcComponentsComponent'
+    });
   }
 });
 
+App.IcComponentsComponentView = Ember.View.extend({
+  didInsertElement: function() {
+    this.$('pre').each(function() {
+      var prettyCode = prettyPrintOne($(this).html());
+      $(this).html(prettyCode);
+    });
+  }
+});
